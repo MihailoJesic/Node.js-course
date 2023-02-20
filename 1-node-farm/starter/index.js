f226d29cfc3ffc1dcfcd3b1bdc9337738a9bc298
mikash91 <mihailo_jesic@yahoo.com>
2023-02-20 17:36:09 +0100

    generate cards

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -34,6 +34,40 @@ const url = require(`url`);
 ////////////////////////////////////////////////////////
 ///Server
 
+function replaceTemplate(temp, product) {
+  console.log(`❌❌❌temp❌❌❌ = `, temp);
+  console.log(`❌❌❌product❌❌❌ = `, product);
+
+  let output = temp.replace(/{% ID %}/g, product.id);
+  output = output.replace(/{% NAME %}/g, product.productName);
+  output = output.replace(/{% IMAGE %}/g, product.image);
+  output = output.replace(/{% FROM %}/g, product.from);
+  output = output.replace(/{% NUTRIENTS %}/g, product.nutrients);
+  output = output.replace(/{% QUANTITY %}/g, product.quantity);
+  output = output.replace(/{% PRICE %}/g, product.price);
+  output = output.replace(/{% DESCRIPTION %}/g, product.description);
+
+  if (!product.organic) {
+    output = output.replace(/{% NOT_ORGANIC %}/g, `not-organic`);
+  }
+
+  console.log(`❌❌❌restult❌❌❌ = `, output);
+  return output;
+}
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  `utf-8`
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  `utf-8`
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  `utf-8`
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
 const dataObj = JSON.parse(data);
 
@@ -41,7 +75,15 @@ const server = http.createServer((req, res) => {
   const path = req.url;
 
   if (path === `/overview` || path === `/`) {
-    res.end(`Hello World!\nThis is the Overview`);
+    res.writeHead(200, { contentType: "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(``);
+
+    const output = tempOverview.replace(`{% CARDS %}`, cardsHtml);
+
+    res.end(output);
   } else if (path === `/product`) {
     res.end(`Hello World!\nThis is the Product`);
   } else if (path === `/api`) {
