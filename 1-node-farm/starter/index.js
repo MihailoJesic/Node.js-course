4c975ba454f98ad0bf9f9c428872a3705d581337
mikash91 <mihailo_jesic@yahoo.com>
2023-02-20 16:33:28 +0100

    API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -5,22 +5,22 @@ const url = require(`url`);
 ////////////////////////////////////////////////////////
 ///Files
 //Synchronous
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textIn = fs.readFileSync("${__dirname}}/txt/input.txt", "utf-8");
 
 // const textOut = `This is what we know about the avocado ${textIn}. \nCreated on ${Date.now()}`;
 
-// fs.writeFileSync(`./txt/output.txt`, textOut);
+// fs.writeFileSync(`${__dirname}}/txt/output.txt`, textOut);
 // console.log(`Text Writen`);
 
 //Asynch
-// fs.readFile(`./txt/start.txt`, `utf-8`, (err, data1) => {
+// fs.readFile(`${__dirname}}/txt/start.txt`, `utf-8`, (err, data1) => {
 //   console.log(data1, err);
-//   fs.readFile(`./txt/${data1}.txt`, `utf-8`, (err, data2) => {
+//   fs.readFile(`${__dirname}}/txt/${data1}.txt`, `utf-8`, (err, data2) => {
 //     console.log(data2);
-//     fs.readFile(`./txt/append.txt`, `utf-8`, (err, data3) => {
+//     fs.readFile(`${__dirname}}/txt/append.txt`, `utf-8`, (err, data3) => {
 //       console.log(data3);
 //       fs.writeFile(
-//         `./txt/output.txt`,
+//         `${__dirname}}/txt/output.txt`,
 //         `${data2}\n\n${data3}\n\n❤`,
 //         `utf-8`,
 //         (err) => {}
@@ -34,7 +34,8 @@ const url = require(`url`);
 ////////////////////////////////////////////////////////
 ///Server
 
-let message = `I'm server!`;
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const path = req.url;
@@ -43,6 +44,9 @@ const server = http.createServer((req, res) => {
     res.end(`Hello World!\nThis is the Overview`);
   } else if (path === `/product`) {
     res.end(`Hello World!\nThis is the Product`);
+  } else if (path === `/api`) {
+    res.writeHead(200, { contentType: "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "content-type": "text/html",
