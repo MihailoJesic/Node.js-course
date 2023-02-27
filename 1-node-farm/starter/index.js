7bd29b9459a071b5d004b5a93ed6de7dd1e8bf57
mikash91 <mihailo_jesic@yahoo.com>
2023-02-27 18:46:51 +0100

    Theory

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -81,5 +81,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, `127.0.0.1`, () => {
-  console.log(`Server 127.0.0.1:8000`);
+  console.log(`Practice Server 127.0.0.1:8000`);
 });
