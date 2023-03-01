1ee014ff99d13f126c2c9e328a76b34c504a8191
mikash91 <mihailo_jesic@yahoo.com>
2023-03-01 16:14:49 +0100

    server listen

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,6 +1,13 @@
 const eventsEmitter = require(`events`);
+const http = require(`http`);
 
-const myEmitter = new eventsEmitter();
+class Sale extends eventsEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sale();
 
 myEmitter.on(`newSale`, () => {
   console.log(`There was a sale!`);
@@ -10,4 +17,30 @@ myEmitter.on(`newSale`, () => {
   console.log(`Customer: Mihailo`);
 });
 
-myEmitter.emit(`newSale`);
+myEmitter.on(`newSale`, (stock) => {
+  console.log(`Event: ${stock} stock sold`);
+});
+
+myEmitter.emit(`newSale`, 9);
+
+////////////////////
+
+const server = http.createServer();
+
+server.on(`request`, (req, res) => {
+  console.log(`Request received`);
+  console.log(req.url);
+  res.end(`Request Received`);
+});
+
+server.on(`request`, (req, res) => {
+  console.log(`Another one ⭕`);
+});
+
+server.on(`close`, (res, req) => {
+  console.log(`Server Closed`);
+});
+
+server.listen(8000, `127.0.0.1`, () => {
+  console.log(`waiting for requests...`);
+});
