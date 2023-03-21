3ab0474e7813543469424aa770572ca3e873fc19
mikash91 <mihailo_jesic@yahoo.com>
2023-03-21 13:08:54 +0100

    Users

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -23,6 +23,10 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
+);
+
 // Route handlers
 
 function getAllTours(req, res) {
@@ -35,6 +39,25 @@ function getAllTours(req, res) {
       tours,
     },
   });
+}
+
+function createTour(req, res) {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 }
 
 function getTour(req, res) {
@@ -57,25 +80,6 @@ function getTour(req, res) {
       tour,
     },
   });
-}
-
-function createTour(req, res) {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
 }
 
 function updateTour(req, res) {
@@ -110,17 +114,107 @@ function deleteTour(req, res) {
   });
 }
 
-// app.get(`/api/v1/tours`, getAllTours);
-// app.get(`/api/v1/tours/:id`, getTour);
-// app.post(`/api/v1/tours`, createTour);
-// app.patch(`/api/v1/tours/:id`, updateTour);
-// app.delete(`/api/v1/tours/:id`, deleteTour);
+function getAllUsers(req, res) {
+  res.status(200).json({
+    status: `success`,
+    requestedAt: req.requestTime,
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+}
+
+function createUser(req, res) {
+  const newId = users[users.length - 1].id + 1;
+  const newUser = Object.assign({ id: newId }, req.body);
+
+  users.push(newUser);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/users.json`,
+    JSON.stringify(users),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          user: newUser,
+        },
+      });
+    }
+  );
+}
+
+function getUser(req, res) {
+  const id = req.params.id * 1;
+
+  const user = users.find((el) => {
+    return el.id === id;
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: `success`,
+    data: {
+      user,
+    },
+  });
+}
+
+function updateUser(req, res) {
+  const id = req.params.id * 1;
+  if (id > users.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: `success`,
+    data: {
+      user: `<Updated user here...>`,
+    },
+  });
+}
+
+function deleteUser(req, res) {
+  const id = req.params.id * 1;
+  if (id > user.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: `success`,
+    data: null,
+  });
+}
 
 // Routes
 
 app.route(`/api/v1/tours`).get(getAllTours).post(createTour);
 
-app.route(`/api/v1/tours/id`).get(getTour).patch(updateTour).delete(deleteTour);
+app
+  .route(`/api/v1/tours/:id`)
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.route(`/api/v1/users`).get(getAllUsers).post(createUser);
+
+app
+  .route(`/api/v1/users/:id`)
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // Start Server
 
