1977de4e3f90e134ef1060f365d64014ca6b4139
mikash91 <mihailo_jesic@yahoo.com>
2023-03-24 15:33:42 +0100

    env vars

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,8 +8,14 @@ const app = express();
 
 // Middleware
 
-app.use(morgan(`dev`));
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === `development`) {
+  app.use(morgan(`dev`));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log(`Hello from middleware ⭕`);
