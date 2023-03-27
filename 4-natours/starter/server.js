0e687f0a3d85ebee0457a4653f72c28de2c3482a
mikash91 <mihailo_jesic@yahoo.com>
2023-03-27 17:14:23 +0200

    DB connection

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -5,19 +5,22 @@ const app = require(`./app`);
 dotenv.config({ path: `./config.env` });
 
 const DB = process.env.DATABASE.replace(
-  `<PASSWORD>`,
+  `<PAASSWORD>`,
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
-});
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`DB Connection Successful`);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}.`);
 });
-
-// MongoDB test
