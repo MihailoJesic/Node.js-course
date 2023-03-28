8e3ef0f47ee596a7c89e6ce4330537682724ff66
mikash91 <mihailo_jesic@yahoo.com>
2023-03-28 15:04:18 +0200

    local db

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -20,6 +20,18 @@ mongoose
     console.log(`DB Connection Successful`);
   });
 
+//Local Database Host Version
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log(`DB Connection Successful`);
+//   });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}.`);
