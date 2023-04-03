bce5498e9f2d3b779c6a49f2fefec622e2c0285c
mikash91 <mihailo_jesic@yahoo.com>
2023-04-03 15:03:44 +0200

    Exclude queries

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -3,9 +3,13 @@ console.log(Tour);
 
 exports.getAllTours = async function (req, res) {
   try {
-    console.log(req.query);
+    const queryObj = { ...req.query };
+    const excludedFields = [`page`, `sort`, `limit`, `fields`];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-    const tours = await Tour.find(req.query);
+    console.log(req.query, queryObj);
+
+    const tours = await Tour.find(queryObj);
 
     res.status(200).json({
       status: `success`,
