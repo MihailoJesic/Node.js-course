67faffd449d901134c64d1110c0eae334c4703a4
mikash91 <mihailo_jesic@yahoo.com>
2023-04-11 16:44:29 +0200

    refactoring

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require(`./../models/tourModel`);
+const APIFeatures = require(`./../utils/apiFeatures`);
 
 exports.aliasTopTours = async function (req, res, next) {
   req.query.limit = `5`;
@@ -9,55 +10,12 @@ exports.aliasTopTours = async function (req, res, next) {
 
 exports.getAllTours = async function (req, res) {
   try {
-    console.log(req.query);
-
-    // Build Query
-    // Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = [`page`, `sort`, `limit`, `fields`];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    //Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(`,`).join(` `);
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort(`-createdAt`);
-    }
-
-    // Field Limiting
-
-    if (req.query.fields) {
-      console.log(`💚 ${req.query.fields}`);
-      query = query.select(req.query.fields.split(',').join(' '));
-    } else {
-      query = query.select(`-__v`);
-    }
-
-    // Pagination
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) {
-        throw new Error(`This page does not exist`);
-      }
-    }
-
-    // Execute Query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // Send Response
     res.status(200).json({
@@ -77,9 +35,6 @@ exports.getAllTours = async function (req, res) {
 
 exports.createTour = async function (req, res) {
   try {
-    // const newTour = new Tour({});
-    // newTour.save()
-
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
@@ -99,7 +54,6 @@ exports.createTour = async function (req, res) {
 exports.getTour = async function (req, res) {
   try {
     const tour = await Tour.findById(req.params.id);
-    // Tour.findOne({ _id: req.params.id });
 
     res.status(200).json({
       status: `success`,
