f04f45a94837b2173d623eece2287861f8352b84
mikash91 <mihailo_jesic@yahoo.com>
2023-05-10 18:06:52 +0200

    move

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
 const mongoose = require(`mongoose`);
 const dotenv = require(`dotenv`);
 
+process.on(`uncaughtException`, (err) => {
+  console.log(err.name, err.message);
+  console.log(`UNCAUGHT EXCEPTION! Shutting down`);
+  process.exit(1);
+});
+
 dotenv.config({ path: `./config.env` });
 const app = require(`./app`);
 
@@ -40,14 +46,6 @@ const server = app.listen(port, () => {
 process.on(`unhandledRejection`, (err) => {
   console.log(err.name, err.message);
   console.log(`UNHANDLED REJECTION! Shutting down`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on(`uncaughtException`, (err) => {
-  console.log(err.name, err.message);
-  console.log(`UNCAUGHT EXCEPTION! Shutting down`);
   server.close(() => {
     process.exit(1);
   });
