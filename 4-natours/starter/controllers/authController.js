1ddfcea7e94829fff70b3d2d5a8039961edb2231
mikash91 <mihailo_jesic@yahoo.com>
2023-05-19 14:03:53 +0200

    token expiration

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require(`util`);
 const jwt = require(`jsonwebtoken`);
 const User = require(`./../models/userModel`);
 const AppError = require(`../utils/appError`);
@@ -61,18 +62,30 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(` `)[1];
   }
 
-  console.log(token);
-
   if (!token) {
     return next(
       new AppError(`You are not logged in! Please log in to get access.`, 401)
     );
   }
   // Token Verification
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // Check if user exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(`The user coresponding to this token no longer exists`, 401)
+    );
+  }
 
   // Check if user changed password after token issuing
+  if (!currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError(`User recently changed password, please log in again`, 401)
+    );
+  }
 
+  // Grant access to protected route
+  req.user = currentUser;
   next();
 });
